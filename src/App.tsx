e5e828e42464f671193547993ed2f3d95b15b110
songhee24 <akaratel61@gmail.com>
2022-02-22 18:08:29 +0600

    Time to Practice: Removing a Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./model/todo";
 import NewTodo from "./components/NewTodo";
 
 function App() {
-    const [todos, setTodos] = useState<Todo[]>([
-        new Todo("Learn React"),
-        new Todo("Learn TypeScript"),
-    ]);
+  const [todos, setTodos] = useState<Todo[]>([
+    new Todo("Learn React"),
+    new Todo("Learn TypeScript"),
+  ]);
 
-    const addTodoHandler = (todoText: string) => {
-        const newTodo = new Todo(todoText);
-        setTodos((prevState) => {
-            return prevState.concat(newTodo);
-        });
-    };
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevState) => {
+      return prevState.concat(newTodo);
+    });
+  };
 
-    return (
-        <div>
-            <NewTodo onAddTodo={addTodoHandler} />
-            <Todos items={todos} />
-        </div>
-    );
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevState) => {
+      return prevState.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+    </div>
+  );
 }
 
-export default App
-
+export default App;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,15 @@
-import classes from './TodoItem.module.css'
+import classes from "./TodoItem.module.css";
+import React from "react";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-    return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: (event: React.MouseEvent) => void;
+}> = (props) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
